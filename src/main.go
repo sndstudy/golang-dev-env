1b54a528c08531217e25baf119053ed48fbd4a68
sndstudy <sanada2054tech@gmail.com>
2020-04-04 22:42:08 +0900

    update: HTTPサーバを作成

--- src/main.go
+++ src/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
+}
 
 func main() {
-	fmt.Printf("Hello World\n")
+	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.ListenAndServe(":18080", nil)
 }
